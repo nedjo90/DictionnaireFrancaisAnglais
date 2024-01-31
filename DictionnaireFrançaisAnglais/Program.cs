0d26c9399d54c53deeff10f1b61bbc2a16dca10d
necati han <han.necati@gmail.com>
2024-01-31 09:15:28 +0100

    Initial commit

---
+++ DictionnaireFrançaisAnglais/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace DictionnaireFrançaisAnglais
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Dictionary<string, string> dictionnaire = new Dictionary<string, string>();
+            StreamReader reader = new StreamReader("/Users/necatihan/RiderProjects/DictionnaireFrançaisAnglais/5000_wordlist_french.csv");
+            string line;
+            while ((line = reader.ReadLine()) != null)
+            {
+                string[] tab = line.Split(',');
+                string key = tab[1];
+                string value = "";
+                for (int i = 2; i < tab.Length; i++)
+                {
+                    value += tab[i] + " ";
+                }
+                dictionnaire.Add(key, value);
+            }
+
+            bool choix = true;
+            while (choix)
+            {
+                Console.Write("Choisir un mot à traduire en anglais (stop pour arrêter le prgramme):\t");
+                string mot = Console.ReadLine();
+                if (mot == "stop")
+                {
+                    choix = false;
+                }
+                else if (dictionnaire[mot] != null)
+                {
+                    Console.WriteLine(dictionnaire[mot]);
+                }
+                else
+                {
+                    Console.WriteLine("Ce mot n'est pas dans le dictionnaire");
+                }
+            }
+        }
+    }
+}
---
+++ DictionnaireFrançaisAnglais/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("DictionnaireFrançaisAnglais")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DictionnaireFrançaisAnglais")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("BD2F2B10-603C-4C6A-9E42-42252B5458A4")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
